6223d2af61e1bff01350ff25f498490ca1ea33db
VetaAgafonova <super.kia.140401@gmail.com>
2022-07-04 09:46:13 +0400

    First commit

---
+++ src/test/java/com/company/saleofspareparts/SaleOfSparePartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.saleofspareparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleOfSparePartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
